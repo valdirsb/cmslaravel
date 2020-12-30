b9a7216b41fe4a515cbb62b64ea16fb3b202e9a2
valdirsb <valdirsb@gmail.com>
2020-12-29 23:35:53 -0300

    alteração no google maps

--- resources/views/layouts/tema1/contato.blade.php
+++ resources/views/layouts/tema1/contato.blade.php
@@ -104,7 +104,9 @@
                                   </div>
                               </div>
                               <div class="col-md-5 order-md-first d-flex align-items-stretch">
-                                  <div id="map" class="map"></div>
+                                  <div  class="map" style="position: relative; overflow: hidden; width:100%">
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2349.378875424913!2d-34.857042477111996!3d-7.981522599919558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab22a92d56ed8f%3A0x97ec20f2a5e612d3!2sClinorte%20Composi%C3%A7%C3%A3o%20e%20Assist%20Tec%20em%20Clich%C3%AAs%20Ltda!5e0!3m2!1spt-BR!2sbr!4v1609292744856!5m2!1spt-BR!2sbr" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                                  </div>
                               </div>
                           </div>
                       </div>
